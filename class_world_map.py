169dbc551dbdc6e9973d9348ef806d3943add422
leehyoseop <64147276+leehyoseop@users.noreply.github.com>
2020-04-29 22:36:58 +0900

    second commit

--- class_world_map.py
+++ class_world_map.py
@@ -4,7 +4,7 @@ from pygame.locals import QUIT
 #pygame.init()
 #SURFACE = pygame.display.set_mode((1000,1000))
 class world_map:
-    def __init__(self):
+    def __init__(self):#init에서 배경생성 
         pygame.init()
         width,height = 1024, 512
         self.gamepad = pygame.display.set_mode((width, height))
@@ -16,7 +16,13 @@ class world_map:
         self.gamepad.blit(background,(0,0))
         pygame.display.update()
         self.clock.tick(60)
-
+    
+    #def 캐릭터관리(좌표, 스킬, 좌 우 점프 공격등등)
+    
+    #def 카메라시점 => 할수있으면
+    
+    #def 
+    
 while True:
     map = world_map()
 
