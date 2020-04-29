465ceb182e59d59c500bcb16b821eb58e4145089
jaewonpark <jaewon29@naver.com>
2020-04-30 06:57:40 +0900

    jaewon1

--- class_option.py
+++ class_option.py
@@ -1,5 +1,15 @@
-import pygame
+import pygame #pyamge 모듈 import
+from pygame.locals import * #pygame.locals 하위 모듈 import
+import keyboard
+from class_main import *
 
 class option:
     def __init__(self):
         self.option = ''
+
+
+class how:
+    def __init__(self):
+        pass
+
+    
