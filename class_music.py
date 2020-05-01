db95772e002ebeba06f0240a165880f36983ad60
Jun Young Yoon <jjun8177@naver.com>
2020-05-01 11:36:19 +0900

    20200501 1136 Jun Yoon

--- class_music.py
+++ class_music.py
@@ -1,7 +1,48 @@
 import pygame
-
-class music:
-    def __init__(self):
-        self.music = ''
-
-print("protein")
+ 
+def playsound(soundfile):
+     
+    pygame.init()
+    pygame.mixer.init()
+    sound= pygame.mixer.Sound(soundfile)
+    clock= pygame.time.Clock()
+    sound.play()
+    while pygame.mixer.get_busy():
+        print("Playing... - func => playsound")
+        clock.tick(1000)
+ 
+def playmusic(soundfile):
+    pygame.init()
+    pygame.mixer.init()
+    clock= pygame.time.Clock()
+    pygame.mixer.music.load(soundfile)
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+        print("Playing... - func => playingmusic")
+        clock.tick(1000)
+         
+def stopmusic():
+    """stop currently playing music"""
+    pygame.mixer.music.stop()
+ 
+def getmixerargs():
+    pygame.mixer.init()
+    freq, size, chan= pygame.mixer.get_init()
+    return freq, size, chan
+ 
+def initMixer():
+    BUFFER = 3072  # audio buffer size, number of samples since pygame 1.8.
+    FREQ, SIZE, CHAN= getmixerargs()
+    pygame.mixer.init(FREQ, SIZE, CHAN,BUFFER)
+             
+try:
+    initMixer()
+    filename= './music/background.ogg'
+    playmusic(filename)
+except KeyboardInterrupt:  # to stop playing, press "ctrl-c"
+    stopmusic()
+    print("\nPlay Stopped by user")
+except Exception:
+    print("unknown error")
+     
+print("Done")
